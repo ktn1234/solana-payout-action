93025c6e32a546411c8a0022efa9c1c1a7528618
ktn1234 <ktn1234@yahoo.com>
2025-05-05 23:36:54 -0500

    chore(config): 🔧 add/configure .commitlintrc file

---
+++ .commitlintrc.ts
@@ -0,0 +1,60 @@
+import type { UserConfig } from "@commitlint/types";
+
+const commitlintConfig: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+  formatter: "@commitlint/format",
+  helpUrl:
+    "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // New feature
+        "fix", // Bug fix
+        "docs", // Documentation changes
+        "style", // Changes that do not affect the meaning of the code (white-space, formatting, etc.)
+        "refactor", // Code changes that neither fix a bug nor add a feature
+        "perf", // Performance improvement
+        "test", // Adding missing tests or correcting existing tests
+        "build", // Changes that affect the build system or external dependencies (example scopes: npm)
+        "ci", // Changes to CI configuration files and scripts
+        "cd", // Changes to CD configuration files and scripts
+        "chore", // Other changes that don't modify src or test files
+        "revert" // Reverts a previous commit
+      ]
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "setup", // Project setup
+        "config", // Configuration files
+        "dependabot", // Dependabot configuration
+        "deps", // Dependency updates
+        "deps-dev", // Development dependency updates
+        "feature", // Feature-specific changes
+        "bug", // Bug fixes
+        "docs", // Documentation
+        "style", // Code style/formatting
+        "refactor", // Code refactoring
+        "test", // Tests
+        "build", // Build scripts or configuration
+        "ci", // Continuous integration
+        "cd", // Continuous deployment
+        "release", // Release related changes
+        "other", // Other changes
+        "solana", // Solana specific changes
+        "actions" // GitHub Actions related changes
+      ]
+    ],
+    "header-max-length": [0],
+    "subject-max-length": [0],
+    "body-max-length": [0],
+    "body-max-line-length": [0],
+    "footer-max-length": [0],
+    "footer-max-line-length": [0]
+  }
+};
+
+export default commitlintConfig;
